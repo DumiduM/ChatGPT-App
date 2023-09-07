c2bee9718fbae8e4c6cc4852117d6545f4e92e64
dumidum <dumindu.desilva@cognite.com>
2023-09-07 12:54:18 +0300

    console based interactivity added

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 import { OpenAIApi, Configuration } from "openai";
 import { config } from "dotenv"
+import readline from "readline"
 
 config()
 
@@ -7,7 +8,22 @@ const configuration = new Configuration({
     organization: "org-M1aW1Urhq8iZOmvK9MfHPkgL",
     apiKey: process.env.OPENAI_API_KEY,
 });
-const openai = new OpenAIApi(configuration);
-const response = await openai.listEngines();
 
-console.log(process.env.OPENAI_API_KEY);
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+userInterface.prompt()
+userInterface.on("line", async input => {
+    const openai = new OpenAIApi(configuration);
+    openai.createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [{ role: "user", content: "Hello ChatGPT"}]
+    })
+    .then(res => {
+        console.log(response.data.choices[0].message.content)
+        userInterface.prompt()
+    })
+})
+
